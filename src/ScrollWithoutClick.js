c17ae50af27d99895220b25b22c138b302350a33
lebrancconvas <lebrancconvas@gmail.com>
2022-02-08 04:56:27 +0700

    🔨 Add Auto Scrolling Function.

--- src/ScrollWithoutClick.js
+++ src/ScrollWithoutClick.js
@@ -11,4 +11,24 @@ const ScrollWithoutClick = async() => {
     await page.goto(link);
 }
 
+
+const autoScrolling = async(page) => {
+    await page.evaluate(async() => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
 module.exports = ScrollWithoutClick;
